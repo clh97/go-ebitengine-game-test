0764419749fdc721611757d519bae647f4df5e9e
Michel Calheiros <michel@calheiros.dev>
2022-08-01 00:35:28 -0300

    added tiled json map support

--- rpg/map.go
+++ rpg/map.go
@@ -1,12 +1,16 @@
 package rpg
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"golang.org/x/image/math/f64"
 	"image"
 	"image/color"
+	"io/ioutil"
 	"log"
+	"os"
 )
 
 const (
@@ -27,6 +31,47 @@ type GameMap struct {
 	sprite *ebiten.Image
 }
 
+type TiledMap struct {
+	Compressionlevel int  `json:"compressionlevel"`
+	Height           int  `json:"height"`
+	Infinite         bool `json:"infinite"`
+	Layers           []struct {
+		Data    []int  `json:"data"`
+		Height  int    `json:"height"`
+		ID      int    `json:"id"`
+		Name    string `json:"name"`
+		Opacity int    `json:"opacity"`
+		Type    string `json:"type"`
+		Visible bool   `json:"visible"`
+		Width   int    `json:"width"`
+		X       int    `json:"x"`
+		Y       int    `json:"y"`
+	} `json:"layers"`
+	Nextlayerid  int    `json:"nextlayerid"`
+	Nextobjectid int    `json:"nextobjectid"`
+	Orientation  string `json:"orientation"`
+	Renderorder  string `json:"renderorder"`
+	Tiledversion string `json:"tiledversion"`
+	Tileheight   int    `json:"tileheight"`
+	Tilesets     []struct {
+		Columns     int    `json:"columns"`
+		Firstgid    int    `json:"firstgid"`
+		Image       string `json:"image"`
+		Imageheight int    `json:"imageheight"`
+		Imagewidth  int    `json:"imagewidth"`
+		Margin      int    `json:"margin"`
+		Name        string `json:"name"`
+		Spacing     int    `json:"spacing"`
+		Tilecount   int    `json:"tilecount"`
+		Tileheight  int    `json:"tileheight"`
+		Tilewidth   int    `json:"tilewidth"`
+	} `json:"tilesets"`
+	Tilewidth int    `json:"tilewidth"`
+	Type      string `json:"type"`
+	Version   string `json:"version"`
+	Width     int    `json:"width"`
+}
+
 func (gm *GameMap) Init() {
 	// w6 h8
 	mapImage, _, err := ebitenutil.NewImageFromFile("assets/sprites/tilesets/plains.png")
@@ -36,6 +81,14 @@ func (gm *GameMap) Init() {
 	}
 
 	gm.sprite = ebiten.NewImageFromImage(mapImage)
+
+	tiles, width, height := gm.readMap()
+
+	gm.MapSizeX = width
+	gm.MapSizeY = height
+	gm.Tiles = [][]Tile{
+		tiles,
+	}
 
 	//gm.Tiles = make([][]Tile, gm.MapSizeY)
 
@@ -93,4 +146,41 @@ func (gm *GameMap) Draw(screen *ebiten.Image) {
 			)
 		}
 	}
+}
+
+func (gm *GameMap) readMap() (tiles []Tile, width, height int) {
+	jsonFile, err := os.Open("demo_map.json")
+
+	if err != nil {
+		log.Fatal("Unable to load map!")
+	}
+
+	defer jsonFile.Close()
+
+	jsonBytes, _ := ioutil.ReadAll(jsonFile)
+
+	var result TiledMap
+
+	json.Unmarshal([]byte(jsonBytes), &result)
+
+	width = result.Width
+	height = result.Height
+
+	gameMap := result.Layers[0]
+	yIndex := 0
+	xIndex := 0
+	for i, tileId := range gameMap.Data {
+		xIndex = i % width
+
+		fmt.Println(i, width, i%width == 0)
+
+		if i%width == 0 {
+			xIndex = 0
+			yIndex++
+		}
+
+		tiles = append(tiles, Tile{Id: tileId - 1, Position: Vector2{X: float64(xIndex), Y: float64(yIndex - 1)}})
+	}
+
+	return tiles, width, height
 }
